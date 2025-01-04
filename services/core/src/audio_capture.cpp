819dd1ee4cc8c5e462ada2c12e546ba69eee2a37
Brendan Miesch <mieschbr@umich.edu>
2025-01-04 10:17:23 -0700

    audio and misc cleanup

--- services/core/src/audio_capture.cpp
+++ services/core/src/audio_capture.cpp
@@ -4,51 +4,47 @@
 
 
 AudioCapture::AudioCapture(unsigned int rate, unsigned int chans)
-    : audio_capture_device_(nullptr), sample_rate_(rate), channels_(chans), format_(SND_PCM_FORMAT_S16_LE) {
+    : audio_capture_device_(nullptr, snd_pcm_close), params_(nullptr),
+      sample_rate_(rate), channels_(chans), format_(SND_PCM_FORMAT_S16_LE) {
     INFO_LOG("Initializing audio capture with rate: " + std::to_string(rate) + " Hz, channels: " + std::to_string(chans));
     InitParams();
 }
 
-AudioCapture::~AudioCapture() {
-    if (audio_capture_device_) {
-        snd_pcm_close(audio_capture_device_);
-        DEBUG_LOG("Audio capture device closed");
-    }
-}
-
 void AudioCapture::InitParams() {
-    int rc = snd_pcm_open(&audio_capture_device_, "default", SND_PCM_STREAM_CAPTURE, 0);
+    snd_pcm_t *raw_device;
+    int rc = snd_pcm_open(&raw_device, "default", SND_PCM_STREAM_CAPTURE, 0);
     if (rc < 0) {
         ERROR_LOG("Cannot open audio device: " + std::string(snd_strerror(rc)));
         throw std::runtime_error("Cannot open audio device: " + std::string(snd_strerror(rc)));
     }
+    audio_capture_device_.reset(raw_device);
 
     // Init parameters
     snd_pcm_hw_params_alloca(&params_);
-    snd_pcm_hw_params_any(audio_capture_device_, params_);
+    snd_pcm_hw_params_any(audio_capture_device_.get(), params_);
 
     // Set parameters
-    rc = snd_pcm_hw_params_set_access(audio_capture_device_, params_, SND_PCM_ACCESS_RW_INTERLEAVED);
+    rc = snd_pcm_hw_params_set_access(audio_capture_device_.get(), params_, SND_PCM_ACCESS_RW_INTERLEAVED);
     if (rc < 0) ERROR_LOG("Cannot set access type: " + std::string(snd_strerror(rc)));
     
-    rc = snd_pcm_hw_params_set_format(audio_capture_device_, params_, format_);
+    rc = snd_pcm_hw_params_set_format(audio_capture_device_.get(), params_, format_);
     if (rc < 0) ERROR_LOG("Cannot set sample format: " + std::string(snd_strerror(rc)));
     
-    rc = snd_pcm_hw_params_set_channels(audio_capture_device_, params_, channels_);
+    rc = snd_pcm_hw_params_set_channels(audio_capture_device_.get(), params_, channels_);
     if (rc < 0) ERROR_LOG("Cannot set channel count: " + std::string(snd_strerror(rc)));
     
-    rc = snd_pcm_hw_params_set_rate_near(audio_capture_device_, params_, &sample_rate_, 0);
+    rc = snd_pcm_hw_params_set_rate_near(audio_capture_device_.get(), params_, &sample_rate_, 0);
     if (rc < 0) ERROR_LOG("Cannot set sample rate: " + std::string(snd_strerror(rc)));
 
     // Apply parameters
-    rc = snd_pcm_hw_params(audio_capture_device_, params_);
+    rc = snd_pcm_hw_params(audio_capture_device_.get(), params_);
     if (rc < 0) {
         ERROR_LOG("Cannot set hardware parameters: " + std::string(snd_strerror(rc)));
         throw std::runtime_error("Cannot set hardware parameters: " + std::string(snd_strerror(rc)));
     }
 
     PrintCurrentParameters();
-    snd_pcm_prepare(audio_capture_device_);
+    snd_pcm_prepare(audio_capture_device_.get());
 }
 
 void AudioCapture::PrintCurrentParameters() {
@@ -69,9 +65,9 @@ void AudioCapture::PrintCurrentParameters() {
 }
 
 void AudioCapture::ResetCaptureDevice() {
-    snd_pcm_drop(audio_capture_device_);
-    snd_pcm_prepare(audio_capture_device_);
-    snd_pcm_reset(audio_capture_device_);
+    snd_pcm_drop(audio_capture_device_.get());
+    snd_pcm_prepare(audio_capture_device_.get());
+    snd_pcm_reset(audio_capture_device_.get());
 }
 
 // TODO: Can this be removed?
@@ -83,12 +79,12 @@ std::vector<int16_t> AudioCapture::CaptureAudio(unsigned int duration_ms) {
 
     snd_pcm_sframes_t frames_read = 0;
     while (frames_read < snd_pcm_sframes_t(frames_to_read)) {
-        snd_pcm_sframes_t rc = snd_pcm_readi(audio_capture_device_, 
+        snd_pcm_sframes_t rc = snd_pcm_readi(audio_capture_device_.get(), 
                                             buffer.data() + frames_read * channels_, 
                                             frames_to_read - frames_read);
         if (rc == -EPIPE) {
             WARN_LOG("Overrun occurred");
-            snd_pcm_prepare(audio_capture_device_);
+            snd_pcm_prepare(audio_capture_device_.get());
         } else if (rc < 0) {
             ERROR_LOG("Error code: " + std::to_string(rc));
             ERROR_LOG("Error description: " + std::string(snd_strerror(rc)));
@@ -101,10 +97,11 @@ std::vector<int16_t> AudioCapture::CaptureAudio(unsigned int duration_ms) {
 }
 
 std::vector<int16_t> AudioCapture::CapturePorcupineFrame() {
+    // 512 frames = 32ms @ 16kHz
     static constexpr int32_t PORCUPINE_FRAME_LENGTH = 512;
     std::vector<int16_t> buffer(PORCUPINE_FRAME_LENGTH);
 
-    int rc = snd_pcm_readi(audio_capture_device_, buffer.data(), PORCUPINE_FRAME_LENGTH);
+    int rc = snd_pcm_readi(audio_capture_device_.get(), buffer.data(), PORCUPINE_FRAME_LENGTH);
 
     if (rc == -EPIPE) {
         WARN_LOG("Overrun occurred");
